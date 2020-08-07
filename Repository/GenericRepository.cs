b58972a9d008d881a99433ed2d884fdd1bc0f770
Hossein Behzadi <behzadi.hosein@yahoo.com>
2020-08-07 13:10:34 +0430

    Add project files.

---
+++ Repository/GenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class GenericRepository<T> where T: class
+    {
+    }
+}
---
+++ Service/GenericService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class GenericService<T> where T : class
+    {
+    }
+}
