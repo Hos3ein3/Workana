302c8da4334ee5f786afbf5001e12c3e6390dcf6
Behdad Nemati <behdad.nemati1@gmail.com>
2020-08-09 15:20:49 +0430

    added required stuff to startup

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data.Context;
+using Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,9 +18,13 @@ namespace Frontend
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment env, IConfiguration configuration)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(env.ContentRootPath);
+            builder.AddJsonFile("Config.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -24,6 +33,54 @@ namespace Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            #region Identity configurations 
+            //(can be separated in IdentityHostingStartup)
+            services.AddDbContext<DBContext>(options =>
+                  options.UseSqlServer(
+                      Configuration.GetConnectionString("DBContextConnection")));
+
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+
+                // Password configurations note: must change DataValidations for ViewModels
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 0;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedAccount = true;
+
+            })
+                .AddRoles<IdentityRole>()
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<DBContext>();
+                //.AddUserManager<CustomUserManager>(); //CustomUserManager class Inherited From UserManger<ApplicationUser> remove if not necessary 
+            #endregion
+
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("SiteAdminPolicy", y => y.RequireRole("SiteAdmins"));
+                x.AddPolicy("ContentManagerPolicy", y => y.RequireRole("ContentManagers"));
+            });
+
+            #region Cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/Identity/Account/Login";
+                options.LogoutPath = $"/Identity/Account/Logout";
+                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+
+                //options.Cookie.HttpOnly = true;
+                //options.ExpireTimeSpan = TimeSpan.FromDays(500);
+                //options.SlidingExpiration = true;
+            });
+            #endregion
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +101,7 @@ namespace Frontend
 
             app.UseRouting();
 
+            app.UseAuthentication(); //enabling Identity
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
